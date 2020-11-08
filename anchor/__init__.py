c0e54222f0ae3cff089f3509506a073434562d6f
Mikael <mikael.honkala@gmail.com>
2020-11-08 12:38:03 +0200

    Rearrange - delete

--- anchor/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Pythonista UI constraints driven by the Key Value Observing (KVO) protocol
-"""
-
-__version__ = '2020.10.22'
-
-from anchors.core import *
-from anchors.safe_area import SafeAreaView
